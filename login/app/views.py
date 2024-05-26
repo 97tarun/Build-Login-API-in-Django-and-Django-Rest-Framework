aea4a4d126d8c33d8d55cd5633699ba42309b4e5
97tarun <105775440+97tarun@users.noreply.github.com>
2024-05-26 09:28:52 +0530

    Add files via upload

---
+++ login/app/views.py
@@ -0,0 +1,10 @@
+from .serializers import UserSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes=(IsAuthenticated,)
+    serializer_class=UserSerializer
+    queryset=get_user_model().objects.all()
